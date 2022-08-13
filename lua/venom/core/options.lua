a8bdd0a51c4577b5d963159e9ccd7f16ae21ef17
Naveen Agarwal <naveenagarwal123451@gmail.com>
2022-08-13 17:38:30 +0530

    add minimal options, keymaps, autocmds

---
+++ lua/venom/core/options.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt
+
+opt.hlsearch = false
+opt.number = true
+opt.relativenumber = true
+opt.hidden = true
+opt.mouse = "a"
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.updatetime = 250
+opt.signcolumn = "yes"
+opt.termguicolors = true
+
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.textwidth = 120
+vim.bo.expandtab = true
+vim.bo.autoindent = true
---
+++ lua/venom/init.lua
@@ -0,0 +1,4 @@
+require("venom.core.keymaps")
+require("venom.core.options")
+require("venom.core.autocmds")
+
